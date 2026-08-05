1b37a380e5ab0b0bbbd75414b56d19fc395d1f4b
TeejMcSteez <teejmcsteez@gmail.com>
2026-08-05 19:43:32 -0400

    update batch size to default from ultralytics (60% of VRAM)

--- train.py
+++ train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     model = YOLO("yolov9s.pt")
-    model.train(data="data.yaml", epochs=100, imgsz=640, batch=16, patience=20)
+    model.train(data="data.yaml", epochs=100, imgsz=640, batch=-1, patience=20)
