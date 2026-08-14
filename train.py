5569d035170e40da58368d6560d739841b6a38d1
TeejMcSteez <teejmcsteez@gmail.com>
2026-08-14 00:26:06 -0400

    from testing frigat max imgsz is 640 training at 1024 provided 0 value

--- train.py
+++ train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     model = YOLO("yolov9s.pt")
-    model.train(data="data.yaml", epochs=200, imgsz=1024, batch=16, patience=40)
+    model.train(data="data.yaml", epochs=200, imgsz=640, batch=16, patience=40)
